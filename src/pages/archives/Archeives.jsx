376881cd31d6c88f5c806c55178795c15af07d02
Gaber Elsayed <47481084+gaberelsayed@users.noreply.github.com>
2024-12-08 00:12:20 +0300

    Update Archeives.jsx

--- src/pages/archives/Archeives.jsx
+++ src/pages/archives/Archeives.jsx
@@ -10,7 +10,7 @@ import TimeAgo from "javascript-time-ago";
 // English.
 import en from "javascript-time-ago/locale/en";
 
-TimeAgo.addDefaultLocale(en);
+TimeAgo.addDefaultLocale(en);     
 
 import "./Archeives.css";
 
